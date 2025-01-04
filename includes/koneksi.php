bda5f80c8b8a500d9f62ebdf118a2552a56b7a0f
dioannda <dioananda275@gmail.com>
2025-01-05 00:18:27 +0700

    add file tugas.php

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -8,7 +8,7 @@ $database = "to_do_list_db";
 // Membuat koneksi ke database
 $koneksi = mysqli_connect($host, $user, $password, $database);
 
-// Cek koneksi
+// Cek koneksi 
 if (!$koneksi) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
